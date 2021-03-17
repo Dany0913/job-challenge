12af0c93ee87ec9b8373c456af357b187879e8f3
Daniela <dany0913@hotmail.com>
2021-03-16 22:19:46 -0500

    Change to the logic

--- API/index.js
+++ API/index.js
@@ -9,15 +9,37 @@ app.use(cors());
 app.get('/jobs',(req,res)=>{
     res.json(jobs);
 })
+
+app.get('/jobs/locations',(req,res)=>{
+    let arrayLocation=[];
+    let arrayLocationUnique=[];
+    jobs.forEach(item => {
+        arrayLocation.concat(item.location)
+    });
+})
+
 app.get('/jobs/filter/:param',(req,res)=>{
+    const param = req.params.param;
+    let filterJob = jobs.filter((item) => {
+        if (item.stack.includes(param)) {
+            return item.stack.includes(param);
+        } else if(item.description.toLowerCase().includes(param)){
+            return item.description;
+        }else {
+            console.log("No match filter")
+        }
+        
+    });  
+    res.json(filterJob);
+})
+
+app.get('/jobs/type/:param',(req,res)=>{
     const param = req.params.param;
     console.log("Param ruta: "+param);
     let filterJob = jobs.filter((item) => {
-        if (param===item.type){
-            console.log(param)
+        if(param.toUpperCase()===item.type.toUpperCase()){
+            console.log(param);
             return param;
-        }else{
-            console.log("No match")
         }
     }); 
     console.log(filterJob);   
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const URL = 'http://localhost:3001/jobs';
 const mainUl = document.querySelector(".mainUl");
 const userForm = document.getElementById("userSearchForm")
+const checkboxTypeFull = document.getElementById("checkboxTypeFull");
+const checkboxTypePart = document.getElementById("checkboxTypePart");
 
 
 const getPost = async() => {
@@ -20,7 +22,6 @@ const getPostFilter = async(param) => {
     try {
         const res = await fetch(`${URL}/filter/${param}`);   
         const jobs = await res.json();
-        console.log(typeof jobs);
         if (Object.values(jobs).length>0) {
             fnClean();
             jobs.forEach(job => {
@@ -34,9 +35,24 @@ const getPostFilter = async(param) => {
     }
 }
 
+const getTypeFilter = async(param) =>{
+    try {
+        const res = await fetch(`${URL}/type/${param}`);   
+        const jobs = await res.json();
+        if (Object.values(jobs).length>0) {
+                jobs.forEach(job => {
+                displayUser(job);
+            });    
+        }else{
+            alert("No se encontraron objetos con tu consulta");
+        } 
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 
 function displayUser(user){
-    console.log(user);
     const li = document.createElement("li");
     li.className="li";
     const divGeneral = document.createElement("div");
@@ -46,36 +62,59 @@ function displayUser(user){
     const divName = document.createElement("div");
     divName.className="div-name";
     const span = document.createElement("span");
+    const spanType = document.createElement("span");
     const img = document.createElement("img");
-    const hr = document.createElement("hr");
     img.src=user.company_logo;
     img.className="user-photo"; //set attribute
-    const userName = document.createElement("h2");
-    const userRole = document.createElement("h4");
-    let captureStatus=document.createTextNode(user.type);
-    const captureUserName = document.createTextNode(`${user.title}`);
-    const captureUserRole = document.createTextNode(user.company);
+    const jobTitle = document.createElement("h2");
+    const jobCompany = document.createElement("h4");
+    let captureStatus=document.createTextNode(`${user.location}   ${user.created_at}`);
+    const captureStatusType=document.createTextNode(user.type);
+    spanType.className="border-span";
+    const captureJobTitle = document.createTextNode(`${user.title}`);
+    const captureJobCompany = document.createTextNode(user.company);
     
     span.appendChild(captureStatus);
-    userName.appendChild(captureUserName);
-    userRole.appendChild(captureUserRole);
+    spanType.appendChild(captureStatusType);
+    jobTitle.appendChild(captureJobTitle);
+    jobCompany.appendChild(captureJobCompany);
     
-    divName.append(userName,userRole);
+    divName.append(jobCompany,jobTitle,spanType);
     divUser.append(img,divName);
     divGeneral.append(divUser,span);
-    li.append(divGeneral,hr);
+    li.append(divGeneral);
     mainUl.appendChild(li);
-    userName.appendChild(captureUserName);
+    jobTitle.appendChild(captureJobTitle);
 }
 
 const fnSearch = (evt)=>{
     evt.preventDefault();
     let inputSearch=document.getElementById("userSearch").value;
-    getPostFilter(inputSearch);
+    getPostFilter(inputSearch.toLowerCase());
+};
+
+const fnCheckboxType = (evt)=>{
+    evt.preventDefault();
+    if (checkboxTypeFull.checked && checkboxTypePart.checked) {
+        getPost();
+    } else if(checkboxTypeFull.checked && !checkboxTypePart.checked){
+        fnClean();
+        getTypeFilter("Full time")
+    }else if(!checkboxTypeFull.checked && checkboxTypePart.checked){
+        fnClean();
+        getTypeFilter("Part time")
+    }else{
+        fnClean();
+    }
+
 };
 
 function fnClean(){
-    mainUl.innerHTML='';
+    // mainUl.innerHTML='';
+    const div=document.querySelectorAll(".div-general");
+    div.forEach(item=>item.remove());
 }
 
 userForm.addEventListener('submit',fnSearch);
+checkboxTypeFull.addEventListener('change',fnCheckboxType);
+checkboxTypePart.addEventListener('change',fnCheckboxType);
